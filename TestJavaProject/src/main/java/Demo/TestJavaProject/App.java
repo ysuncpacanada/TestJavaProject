6d937abfc989f963ed44028ddb63724593cad06e
ysuncpacanada <70352043+ysuncpacanada@users.noreply.github.com>
2021-11-16 18:42:00 -0800

    First commit

---
+++ TestJavaProject/src/main/java/Demo/TestJavaProject/App.java
@@ -0,0 +1,14 @@
+package Demo.TestJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
